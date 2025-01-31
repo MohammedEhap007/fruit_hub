610d6eca91932c02bd180f545face540541038cb
MohammedEhap007 <mohammedehap007@gmail.com>
2025-02-01 00:01:06 +0200

    create custom button

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:fruit_hub_app/core/utils/app_colors.dart';
+import 'package:fruit_hub_app/core/widgets/custom_button.dart';
 import 'package:fruit_hub_app/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -17,7 +18,20 @@ class OnBoardingViewBody extends StatelessWidget {
             activeColor: AppColors.primaryColor,
             color: AppColors.primaryColor.withAlpha(128),
           ),
-        )
+        ),
+        SizedBox(
+          height: 29,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: CustomButton(
+            onPressed: () {},
+            text: 'ابدأ الان',
+          ),
+        ),
+        SizedBox(
+          height: 43,
+        ),
       ],
     );
   }
