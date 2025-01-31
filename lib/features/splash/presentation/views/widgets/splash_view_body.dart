2779f5747a0b130a27d82cc6c3e8063ee9a89375
MohammedEhap007 <mohammedehap007@gmail.com>
2025-01-31 20:04:52 +0200

    splash view logic done

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fruit_hub_app/core/utils/app_images.dart';
+import 'package:fruit_hub_app/features/on_boarding/presentation/views/on_boarding_view.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    super.initState();
+    executeNavigation();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +35,17 @@ class SplashViewBody extends StatelessWidget {
           fit: BoxFit.fill,
         ),
       ],
+    );
+  }
+
+  void executeNavigation() {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        if (mounted) {
+          Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+        }
+      },
     );
   }
 }
