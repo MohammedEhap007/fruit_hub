2e47a67d5ac53e673bb10a6b90ec6fcf23ae6de9
MohammedEhap007 <mohammedehap007@gmail.com>
2025-02-01 02:54:07 +0200

    add text styles

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub_app/core/utils/app_text_styles.dart';
 import 'package:fruit_hub_app/features/on_boarding/presentation/views/widgets/page_view_item.dart';
 
+import '../../../../../core/utils/app_colors.dart';
 import '../../../../../core/utils/app_images.dart';
 
 class OnBoardingPageView extends StatelessWidget {
@@ -20,9 +22,22 @@ class OnBoardingPageView extends StatelessWidget {
           title: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('مرحبًا بك في'),
-              Text('Fruit'),
-              Text('HUB'),
+              Text(
+                'مرحبًا بك في',
+                style: TextStyles.bold23,
+              ),
+              Text(
+                'HUB',
+                style: TextStyles.bold23.copyWith(
+                  color: AppColors.secondaryColor,
+                ),
+              ),
+              Text(
+                'Fruit',
+                style: TextStyles.bold23.copyWith(
+                  color: AppColors.primaryColor,
+                ),
+              ),
             ],
           ),
           subtitle:
@@ -35,7 +50,10 @@ class OnBoardingPageView extends StatelessWidget {
           title: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('ابحث وتسوق'),
+              Text(
+                'ابحث وتسوق',
+                style: TextStyles.bold23,
+              ),
             ],
           ),
           subtitle:
--- lib/features/on_boarding/presentation/views/widgets/page_view_item.dart
+++ lib/features/on_boarding/presentation/views/widgets/page_view_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruit_hub_app/core/utils/app_text_styles.dart';
 
 class PageViewItem extends StatelessWidget {
   const PageViewItem({
@@ -42,8 +43,11 @@ class PageViewItem extends StatelessWidget {
                 visible: isVisible,
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
-                  child: const Text(
+                  child: Text(
                     'تخط',
+                    style: TextStyles.regular13.copyWith(
+                      color: const Color(0XFF949D9E),
+                    ),
                   ),
                 ),
               ),
@@ -59,10 +63,12 @@ class PageViewItem extends StatelessWidget {
         ),
         Padding(
           padding: const EdgeInsets.symmetric(
-            horizontal: 16.0,
+            horizontal: 37.0,
           ),
           child: Text(
             subtitle,
+            style:
+                TextStyles.semiBold13.copyWith(color: const Color(0XFF4E5456)),
             textAlign: TextAlign.center,
           ),
         )
