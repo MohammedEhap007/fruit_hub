b5d1b2035fa6de2c624b73c36f6ac00fcaff3574
MohammedEhap007 <mohammedehap007@gmail.com>
2025-04-11 00:16:08 +0200

    create custom check box & terms and conditions widget

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub_app/constants.dart';
+import 'package:fruit_hub_app/core/widgets/custom_text_form_field.dart';
+import 'package:fruit_hub_app/features/auth/presentation/views/widgets/terms_and_conditions_widget.dart';
 
 class SignupViewBody extends StatelessWidget {
   const SignupViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: kHorizontalPadding,
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 24,
+            ),
+            CustomTextFormField(
+              keyboardType: TextInputType.name,
+              hintText: 'الاسم كامل',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFormField(
+              keyboardType: TextInputType.emailAddress,
+              hintText: 'البريد الإلكتروني',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFormField(
+              keyboardType: TextInputType.visiblePassword,
+              hintText: 'كلمة المرور',
+              suffixIcon: Icon(
+                Icons.remove_red_eye,
+                color: Color(0XFFC9CECF),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            TermsAndConditionsWidget(
+              onChanged: (value) {},
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
