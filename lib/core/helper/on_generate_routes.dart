656afa81377112be1e37e5cd1c2affc39aec87a3
MohammedEhap007 <mohammedehap007@gmail.com>
2025-04-10 23:42:26 +0200

    create signup view

--- lib/core/helper/on_generate_routes.dart
+++ lib/core/helper/on_generate_routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub_app/features/auth/presentation/views/signup_view.dart';
 
 import '../../features/auth/presentation/views/login_view.dart';
 import '../../features/on_boarding/presentation/views/on_boarding_view.dart';
@@ -12,6 +13,8 @@ Route onGenerateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => const OnBoardingView());
     case LoginView.routeName:
       return MaterialPageRoute(builder: (context) => const LoginView());
+    case SignupView.routeName:
+      return MaterialPageRoute(builder: (context) => const SignupView());
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }
--- lib/features/auth/presentation/views/widgets/dont_have_an_account_widget.dart
+++ lib/features/auth/presentation/views/widgets/dont_have_an_account_widget.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:fruit_hub_app/features/auth/presentation/views/signup_view.dart';
 
 import '../../../../../core/utils/app_colors.dart';
 import '../../../../../core/utils/app_text_styles.dart';
@@ -20,6 +22,10 @@ class DontHaveAnAccountWidget extends StatelessWidget {
             ),
           ),
           TextSpan(
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                Navigator.pushNamed(context, SignupView.routeName);
+              },
             text: 'قم بإنشاء حساب',
             style:
                 TextStyles.semiBold16.copyWith(color: AppColors.primaryColor),
