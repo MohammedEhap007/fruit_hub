eb593cf1c8b80a918c61b5eb33a0ee2fda6c4537
MohammedEhap007 <mohammedehap007@gmail.com>
2025-04-11 03:48:37 +0200

    create user entity and auth repo

---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruit_hub_app/core/errors/failures.dart';
+import 'package:fruit_hub_app/features/auth/domain/entites/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+    String email,
+    String password,
+  );
+}
