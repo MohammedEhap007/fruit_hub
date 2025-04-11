c7d7c5ca1bf1dbef33a67e3fb5a4495c1d0c93d2
MohammedEhap007 <mohammedehap007@gmail.com>
2025-04-11 20:00:49 +0200

    create custom bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub_app/core/helper/on_generate_routes.dart';
+import 'package:fruit_hub_app/core/services/custom_bloc_observer.dart';
 import 'package:fruit_hub_app/core/services/get_it_service.dart';
 import 'package:fruit_hub_app/core/services/shared_preferences_singleton.dart';
 import 'package:fruit_hub_app/core/utils/app_colors.dart';
@@ -12,6 +14,7 @@ import 'package:fruit_hub_app/generated/l10n.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = CustomBlocObserver();
   await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
