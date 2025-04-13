cffa7e795cf2de4ce12f1ca54fd53bef25619e47
MohammedEhap007 <mohammedehap007@gmail.com>
2025-04-13 22:36:20 +0200

    create signin bloc consumer and custom progress hud

---
+++ lib/features/auth/presentation/views/signin_view_body_bloc_consumer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub_app/core/widgets/custom_progress_hud.dart';
+import 'package:fruit_hub_app/features/auth/presentation/cubits/signin_cubit/signin_cubit.dart';
+import 'package:fruit_hub_app/features/auth/presentation/views/widgets/signin_view_body.dart';
+
+class SigninViewBodyBlocConsumer extends StatelessWidget {
+  const SigninViewBodyBlocConsumer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<SigninCubit, SigninState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return CustomProgressHud(
+          isLoading: state is SigninLoading ? true : false,
+          child: const SigninViewBody(),
+        );
+      },
+    );
+  }
+}
