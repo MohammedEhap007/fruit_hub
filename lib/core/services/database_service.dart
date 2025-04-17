00c0fa537a68f5e4431407e28eada13f59aa3cc2
MohammedEhap007 <mohammedehap007@gmail.com>
2025-04-17 21:56:57 +0200

    create and implement get user data

--- lib/core/services/database_service.dart
+++ lib/core/services/database_service.dart
@@ -1,6 +1,13 @@
+import 'package:fruit_hub_app/features/auth/domain/entities/user_entity.dart';
+
 abstract class DatabaseService {
   Future<void> addData({
     required String path,
     required Map<String, dynamic> data,
+  });
+
+  Future<UserEntity> getUserData({
+    required String path,
+    required String uId,
   });
 }
--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fruit_hub_app/core/services/database_service.dart';
+import 'package:fruit_hub_app/features/auth/data/models/user_model.dart';
+import 'package:fruit_hub_app/features/auth/domain/entities/user_entity.dart';
 
 class FirestoreService implements DatabaseService {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -10,5 +12,14 @@ class FirestoreService implements DatabaseService {
     required Map<String, dynamic> data,
   }) async {
     await firestore.collection(path).add(data);
+  }
+
+  @override
+  Future<UserEntity> getUserData({
+    required String path,
+    required String uId,
+  }) async {
+    var data = await firestore.collection(path).doc(uId).get();
+    return UserModel.fromJson(data.data()!);
   }
 }
