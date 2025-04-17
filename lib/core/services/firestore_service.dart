c88a34eed22a7766ac8ddc4cc0c6ead23d4ae88c
MohammedEhap007 <mohammedehap007@gmail.com>
2025-04-17 17:47:16 +0200

    create firestore service class

---
+++ lib/core/services/firestore_service.dart
@@ -0,0 +1,10 @@
+import 'package:fruit_hub_app/core/services/database_service.dart';
+
+class FirestoreService implements DatabaseService {
+  @override
+  Future<void> addData(
+      {required String path, required Map<String, dynamic> data}) {
+    // TODO: implement addData
+    throw UnimplementedError();
+  }
+}
