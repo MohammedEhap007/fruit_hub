0d878009df287d8c7aea61eeb5b0ec08f69dca7a
MohammedEhap007 <mohammedehap007@gmail.com>
2025-04-20 22:35:38 +0200

    create featured item

---
+++ lib/features/home/presentation/views/widgets/featured_item_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub_app/core/utils/app_colors.dart';
+import 'package:fruit_hub_app/core/utils/app_text_styles.dart';
+
+class FeaturedItemButton extends StatelessWidget {
+  const FeaturedItemButton({
+    super.key,
+    this.onPressed,
+  });
+
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 32,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24.0,
+          ),
+          child: FittedBox(
+            child: Text(
+              'تسوق الان',
+              style: TextStyles.bold13.copyWith(
+                color: AppColors.primaryColor,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fruit_hub_app/constants.dart';
 import 'package:fruit_hub_app/core/widgets/search_text_field.dart';
 import 'package:fruit_hub_app/features/home/presentation/views/widgets/custom_home_app_bar.dart';
+import 'package:fruit_hub_app/features/home/presentation/views/widgets/featured_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -25,6 +26,10 @@ class HomeViewBody extends StatelessWidget {
                   height: 16.0,
                 ),
                 SearchTextField(),
+                SizedBox(
+                  height: 12.0,
+                ),
+                FeaturedItem(),
               ],
             ),
           ),
